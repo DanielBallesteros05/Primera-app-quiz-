58be8d5ef65e3cdc490d60641cfe13a07ebefe31
danielb <carlos.ballesteros8937@alumnos.udg.mx>
2023-01-24 22:15:01 -0600

    Mi primer App

---
+++ app/src/main/java/ballesteros/daniel/com/example/primeraappquiz/MainActivity.kt
@@ -0,0 +1,11 @@
+package ballesteros.daniel.com.example.primeraappquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
